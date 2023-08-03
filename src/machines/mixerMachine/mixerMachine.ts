70a50dcde57fc42ff544a56f7d86d420f6ed0b42
Julio Saito <saitodisse@gmail.com>
2023-08-02 22:43:12 -0300

    mixerMachine: add history

--- src/machines/mixerMachine/mixerMachine.ts
+++ src/machines/mixerMachine/mixerMachine.ts
@@ -2,20 +2,22 @@ import { createMachine } from "xstate";
 
 export const mixerMachine = createMachine(
   {
-    /** @xstate-layout N4IgpgJg5mDOIC5QFkCWAPMAnAdBOANqlAIYQD2AxADICSA4gIIBKA2gAwC6ioADubFQAXVOQB2PEOkQBGAJzscAZgAc7GQFYNANgBM2mQBYZ+gDQgAnokPaNOOUoO6A7M9U2Nz7QF9v5tJi4RKQUlAAiAKIAynRMbFyS-IIi4pLSCDIqMjgqBipKSkYqhhomhuZWCHI47AUqChpyupoySnKGvv4Y2DjBZOQ4-EJgYgDGqCQylIwAqsgRAHIAKiwc3EggScKiEhvpGiX2B0rq7drt7OWWiEq6ujiNhs6Gquzs2qraHX4gAT19FEG5GGYwmummc0WK3i6z4Am2qT2iCy9ye+XYXheKmccmcFRudweZzkuI09W07DknV+3SCxH6QJB4xI4LCtGQtAWM1oMMS8JSu1A+wONVUzlKzhkGLkmnxGWc9zUSjctjUd081L+dJCAyGI2ZSnC7M53N5Gy2ArSyJOor0MnkBnF7F0cpkCpytRVZOdug11LE5Hw8A2Wr5yR2VoQAFptHKo84VDhDC8Lip8l4dEpNbS8IR6RQwwjBVJrC7rghsQ9PNoa211I4NLps4FevnyIXLUiEI5sh8VLpcSdSoZKRo5So7LcycU3RpKROZM3-m3GfqJjIOxGuyPqn2B252MPR3LdIZEzpKSdsTK+0vtQy9aCWZvEULENpxfYmkYvFKB6oTzPB4KVxQwmnYGc-S6FsAV1YE1xIJQX2LdIZXuNoB00Zo5y0PFy1Pc8QOecDIOcXxfCAA */
+    /** @xstate-layout N4IgpgJg5mDOIC5QFkCWAPMAnAdBOANqlAIYQD2AxADICSA4gIIBKA2gAwC6ioADubFQAXVOQB2PEOkQBGAJzscAZgAc7GQFYNANgBM2mQBYZ+gDQgAnokPaNOOUoO6A7M9U2Nz7QF9v5tJi4RKQUlAAiAKIAynRMbFyS-IIi4pLSCDIqMjgqBipKSkYqhhomhuZWCHI47AUqChpyupoySnKGvv4Y2DjBZOQ4-EJgYgDGqCQylIwAqsgRAHIAKiwc3EggScKiEhvpGiX2B0rq7drt7OWWiEq6ujiNhs6Gquzs2qraHX4gAT19FEG5GGYwmummc0WK3i6z4Am2qT2iCy9ye+XYXheKmccmcFRudweZzkuI09W07DknV+3SCxH6QJB4xI4LCtGQtAWM1oMMS8JSu1A+wONVUzlKzhkGLkmnxGWc9zUSjctjUd081L+dJCAyGI2ZSnC7M53N5Gy2ArSyJOor0MnkBnF7F0cpkCpytRVZOdug11LE5Hw8A2Wr5yR2VoQAFptHKo84VDhDC85J4bOwVC4TJraXhCPSKGGEYKpNYXdcENiHp5tLW2upHBpdDnAr0C+Qi5akQhdHJtMp5H3ik9JTK5So7LcySoJy5dE83S3-u2cAALVCwISdiPdpTJgck7TD1zyGRywyEhXqNwyzQGGxL7UMvWgybbxFC6yUnAfTO4k6lIYlIaHK86JjolInNiMq-o+bY6oy+pgu+JbpNo4r2E0RheFKvaqKBhjgRSuKGE0GbGH6XStgCurAkhJBKChkYyvcbS9pozQaOwWh4hWYEPMRzxkcUmjOL4vhAA */
     id: "Mixer",
     initial: "desligado",
     states: {
       desligado: {
         on: {
-          LIGAR: {
-            target: "#Mixer.ligado.potencia1",
-          },
+          LIGAR: "ligado.hist",
         },
       },
       ligado: {
         initial: "potencia1",
+
         states: {
+          hist: {
+            type: "history",
+          },
           potencia1: {
             on: {
               AUMENTAR: {
