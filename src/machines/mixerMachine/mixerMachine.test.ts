f6b2ebaac340a86c6aee2511a8dee13a51de55a9
Julio Saito <saitodisse@gmail.com>
2023-11-11 01:34:30 -0300

    add tests to all states on mixerMachine

--- src/machines/mixerMachine/mixerMachine.test.ts
+++ src/machines/mixerMachine/mixerMachine.test.ts
@@ -1,6 +1,6 @@
 import { createActor } from 'xstate'
 import { machine } from './mixerMachine'
-import { test } from 'vitest'
+import { expect, test } from 'vitest'
 import { printSnapshot } from '../../utils/tests/printSnapshot'
 
 test('mixerMachine createActorer', async () => {
@@ -24,18 +24,49 @@ test('mixerMachine createActorer', async () => {
   actor.start()
 
   actor.send({ type: 'TURN_ON' })
+  expect(actor.getSnapshot().value).toEqual({ turned_on: 'power_level_1' })
+
   actor.send({ type: 'INCREASE_POWER' })
+  expect(actor.getSnapshot().value).toEqual({ turned_on: 'power_level_2' })
+
   actor.send({ type: 'INCREASE_POWER' })
+  expect(actor.getSnapshot().value).toEqual({ turned_on: 'power_level_3' })
+
   actor.send({ type: 'DECREASE_POWER' })
+  expect(actor.getSnapshot().value).toEqual({ turned_on: 'power_level_2' })
+
+  actor.send({ type: 'DECREASE_POWER' })
+  expect(actor.getSnapshot().value).toEqual({ turned_on: 'power_level_1' })
+
+  // NO EFFECT
+  actor.send({ type: 'DECREASE_POWER' })
+  expect(actor.getSnapshot().value).toEqual({ turned_on: 'power_level_1' })
+
+  actor.send({ type: 'INCREASE_POWER' })
+  expect(actor.getSnapshot().value).toEqual({ turned_on: 'power_level_2' })
 
   actor.send({ type: 'TURN_OFF' })
+  expect(actor.getSnapshot().value).toEqual('turned_off')
+
+  // History remembers the last state: power_level_2
   actor.send({ type: 'TURN_ON' })
+  expect(actor.getSnapshot().value).toEqual({ turned_on: 'power_level_2' })
 
   actor.send({ type: 'INCREASE_POWER' })
+  expect(actor.getSnapshot().value).toEqual({ turned_on: 'power_level_3' })
+
+  // NO EFFECT
+  actor.send({ type: 'INCREASE_POWER' })
+  expect(actor.getSnapshot().value).toEqual({ turned_on: 'power_level_3' })
+
   actor.send({ type: 'DECREASE_POWER' })
+  expect(actor.getSnapshot().value).toEqual({ turned_on: 'power_level_2' })
+
   actor.send({ type: 'DECREASE_POWER' })
+  expect(actor.getSnapshot().value).toEqual({ turned_on: 'power_level_1' })
 
   actor.send({ type: 'TURN_OFF' })
+  expect(actor.getSnapshot().value).toEqual('turned_off')
 
   subscription.unsubscribe()
 }, 50000)
